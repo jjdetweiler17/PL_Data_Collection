e309e2f2f73e47021abe9f4a0492dd2c1949cc47
jjdetweiler17 <john.detweiler@sps.edu>
2021-07-25 21:02:37 -0400

    Initial Commit

--- Programs/VG_Full_Stack_C.py
+++ Programs/VG_Full_Stack_C.py
@@ -31,6 +31,49 @@ def reformat_text(text):
     return text
 
 
+def extract_data_sub(page, fund_name):
+    page = reformat_text(page)
+    data_list = []
+    block_string = page
+    while block_string.count("\n") != 0:
+        data_list.append(block_string[:block_string.find("\n")])
+        block_string = block_string[block_string.find("\n") + 1:]
+    for x in range(len(data_list)):
+        data_list[x] = data_list[x].strip()
+        if data_list[x].count("class") != 0:
+            ind = data_list[x].find("class")
+            if data_list[x][:ind].count("  ") != 0:
+                double_space_ind = data_list[x].rfind("  ", 0, ind)
+                string1 = data_list[x][:double_space_ind]
+                data_list[x] = data_list[x][double_space_ind + 2:]
+                data_list.insert(x, string1)
+                x += 1
+        data_list[x] = data_list[x].replace("  ", " ")
+        if data_list[x].count("(") != 0:
+            start_ind = data_list[x].rfind("(")
+            end_ind = data_list[x].rfind(")")
+            data_list[x] = data_list[x][:start_ind] + data_list[x][end_ind + 1:]
+
+        if data_list[x].count("(") != 0:
+            start_ind = data_list[x].rfind("(")
+            end_ind = data_list[x].rfind(")")
+            data_list[x] = data_list[x][:start_ind] + data_list[x][end_ind + 1:]
+    data_list_dict = {}
+    for string in data_list:
+        hasnum = False
+        for char in string:
+            if char.isnumeric():
+                hasnum = True
+
+        if hasnum:
+            space_index = string.rfind(" ")
+            data_list_dict[string[:space_index].strip(" $")] = string[space_index + 1:].replace(",", "")
+        else:
+            data_list_dict[string] = ""
+    df = pd.DataFrame(list(data_list_dict.items()), columns=['Statement of Operations', fund_name.title()])
+    return df
+
+
 def extract_data(path):
     # put each page's string into a list
     page_list = []
@@ -56,13 +99,17 @@ def extract_data(path):
         par1_ind = page_list[0].find("(")
         par2_ind = page_list[0].find(")")
         page_list[0] = page_list[0][:par1_ind] + page_list[0][par2_ind + 1:]
-    for i in range(page_list[0].count("fund")):
+    for a in range(page_list[0].count("fund")):
         print("a")
         fund_ind = page_list[0].find("fund", last_ind + 1)
         last_ind = fund_ind + 1
         nl_ind = page_list[0].rfind("\n", 0, fund_ind)
-        if page_list[0][fund_ind+4] != "s":
+        if fund_ind + 4 < len(page_list[0]) and page_list[0][fund_ind+4] != "s":
             fund_name_list.append(page_list[0][nl_ind+1:fund_ind+4])
+    if fund_name_list.count("toyourfund") != 0:
+        fund_name_list.remove("toyourfund")
+    if fund_name_list.count("to your fund") != 0:
+        fund_name_list.remove("to your fund")
     print(fund_name_list)
     statements_page_list = []
     for page in page_list:
@@ -73,52 +120,30 @@ def extract_data(path):
             statements_page_list.append(page)
         elif page.find("statement of operations", 0, nl_ind) != -1:
             statements_page_list.append(page)
-    for i in range(len(statements_page_list)):
-        statements_page_list[i] = reformat_text(statements_page_list[i])
-        data_list = []
-        block_string = statements_page_list[i]
-        while block_string.count("\n") != 0:
-            data_list.append(block_string[:block_string.find("\n")])
-            block_string = block_string[block_string.find("\n") + 1:]
-        for i in range(len(data_list)):
-            data_list[i] = data_list[i].strip()
-            if data_list[i].count("class") != 0:
-                ind = data_list[i].find("class")
-                if data_list[i][:ind].count("  ") != 0:
-                    double_space_ind = data_list[i].rfind("  ", 0, ind)
-                    string1 = data_list[i][:double_space_ind]
-                    data_list[i] = data_list[i][double_space_ind + 2:]
-                    data_list.insert(i, string1)
-                    i += 1
-            data_list[i] = data_list[i].replace("  ", " ")
-            if data_list[i].count("(") != 0:
-                start_ind = data_list[i].rfind("(")
-                end_ind = data_list[i].rfind(")")
-                data_list[i] = data_list[i][:start_ind] + data_list[i][end_ind + 1:]
-
-            if data_list[i].count("(") != 0:
-                start_ind = data_list[i].rfind("(")
-                end_ind = data_list[i].rfind(")")
-                data_list[i] = data_list[i][:start_ind] + data_list[i][end_ind + 1:]
-            data_list_dict = {}
-            for string in data_list:
-                hasnum = False
-                for char in string:
-                    if char.isnumeric():
-                        hasnum = True
-
-                if hasnum:
-                    space_index = string.rfind(" ")
-                    data_list_dict[string[:space_index].strip(" $")] = string[space_index + 1:].replace(",", "")
-                else:
-                    data_list_dict[string] = ""
-
-            df = pd.DataFrame(list(data_list_dict.items()), columns=['Statement of Operations', fund_name.title()])
-            print(df)
-            return df
+    print(type(fund_name_list))
+    if fund_name_list is None or len(fund_name_list) < 2:
+        df = extract_data_sub(statements_page_list[0], fund_name)
+    else:
+        df = extract_data_sub(statements_page_list[0], fund_name_list[0])
+    if len(statements_page_list) >= 2:
+        for a in range(len(statements_page_list) - 2):
+            if fund_name_list is None or len(fund_name_list) < 2:
+                df = pd.merge(df, extract_data_sub(statements_page_list[a+2], fund_name), how="outer")
+            else:
+                df = pd.merge(df, extract_data_sub(statements_page_list[a+2],
+                                                   fund_name_list[a // 2]), how="outer")
+    return df
 
 
+count = 0
+extract_data(pdf_list[25])
+pdf_list = os.listdir(path1)
+pdf_list = pdf_list[20:30]
+print(len(pdf_list))
+full_df = pd.merge(extract_data(pdf_list[0]), extract_data(pdf_list[1]), how='outer')
+for i in range(len(pdf_list) - 2):
+    full_df = pd.merge(full_df, extract_data(pdf_list[i+2]), how='outer')
+    print(count)
+    count += 1
 
-
-
-extract_data("Cash_Reserves_Federal_Money_Market_Fund_Admiral__annualReport.pdf")
+full_df.to_csv(r'C:\Users\jjdet\Desktop\PL_Data_Collection\full_VG_spreadsheet.csv')
